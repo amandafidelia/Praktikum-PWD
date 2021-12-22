a6b530b757bc7dae10ab7c744a91dd9fe0c80f73
amandafidelia <amanda1900018154@webmail.uad.ac.id>
2021-12-22 19:49:56 +0700

    praktikum pwd 10

---
+++ pertemuan10/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$host="localhost"; //untuk hostnya 
+$username="root"; //untuk username nya
+$password=""; //untuk passwordnya, yaitu kosong
+$databasename="akademik2"; //nama db nya, saya memakai akademik2
+
+//sintak untuk mengkoneksikan ke database nya
+$con=@mysqli_connect($host,$username,$password,$databasename);
+if (!$con) {
+//jika gagal, maka akan menampilkan errornya
+ echo "Error: " . mysqli_connect_error();
+exit();
+}
+?>
